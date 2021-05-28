4304248585a63d761e88cc4fa5d4d19ae49aaf83
saderdesigner <saderdesigner@gmail.com>
2021-05-29 03:45:09 +0700

    final release

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,128 +1,131 @@
 (async () => {
-    // Load model
-    console.log("1");
-    // await faceapi.nets.faceRecognitionNet.loadFromUri("../models");
-    // await faceapi.nets.faceLandmark68Net.loadFromUri("../models");
+  // Load model
+  console.log('1');
+  // await faceapi.nets.faceRecognitionNet.loadFromUri("../models");
+  // await faceapi.nets.faceLandmark68Net.loadFromUri("../models");
 
-    // await faceapi.nets.ssdMobilenetv1.loadFromUri('../models');
-    // await faceapi.nets.tinyFaceDetector.loadFromUri('/models');
-    // await faceapi.nets.faceLandmark68Net.loadFromUri('/models');
-    // await faceapi.nets.faceRecognitionNet.loadFromUri('/models');
-    // await faceapi.nets.faceExpressionNet.loadFromUri('/models');
-    // console.log('2')
-    // // Detect Face
-    // const input = document.getElementById("myImg");
+  await faceapi.nets.ssdMobilenetv1.loadFromUri('../models');
+  await faceapi.nets.tinyFaceDetector.loadFromUri('/models');
+  await faceapi.nets.faceLandmark68Net.loadFromUri('/models');
+  await faceapi.nets.faceRecognitionNet.loadFromUri('/models');
+  await faceapi.nets.faceExpressionNet.loadFromUri('/models');
+  // console.log('2')
+  // // Detect Face
+  // const input = document.getElementById("myImg");
 
-    // const result = await faceapi
-    //   .detectSingleFace(input, new faceapi.SsdMobilenetv1Options())
-    //   .withFaceLandmarks()
-    //   .withFaceDescriptor();
+  // const result = await faceapi
+  //   .detectSingleFace(input, new faceapi.SsdMobilenetv1Options())
+  //   .withFaceLandmarks()
+  //   .withFaceDescriptor();
 
-    //   console.log('3')
-    // const displaySize = { width: input.width, height: input.height };
-    // // resize the overlay canvas to the input dimensions
-    // const canvas = document.getElementById("myCanvas");
-    // faceapi.matchDimensions(canvas, displaySize);
-    // const resizedDetections = faceapi.resizeResults(result, displaySize);
-    // console.log(resizedDetections);
+  //   console.log('3')
+  // const displaySize = { width: input.width, height: input.height };
+  // // resize the overlay canvas to the input dimensions
+  // const canvas = document.getElementById("myCanvas");
+  // faceapi.matchDimensions(canvas, displaySize);
+  // const resizedDetections = faceapi.resizeResults(result, displaySize);
+  // console.log(resizedDetections);
 
-    /**
-     * Real time face detection
-     */
+  /**
+   * Real time face detection
+   */
 
-    // Recognize Face
-    // let labeledFaceDescriptors = JSON.parse(localStorage.getItem('labeledFaceDescriptors'));
-    // console.log('1', labeledFaceDescriptors);
-    // if (!labeledFaceDescriptors){
+  // Recognize Face
+  // let labeledFaceDescriptors = JSON.parse(localStorage.getItem('labeledFaceDescriptors'));
+  // console.log('1', labeledFaceDescriptors);
+  // if (!labeledFaceDescriptors){
 
-    let response = await fetch("http://localhost:3002/web/getModel/", {
-        method: "GET",
-    });
+  let response = await fetch('http://localhost:3002/web/getModel/', {
+    method: 'GET',
+  });
 
-    response = await response.json();
+  response = await response.json();
 
-    console.log("response: ", response);
-    // }
-    const labeledFaceDescriptors = response.model;
+  console.log('response: ', response);
+  // }
+  const labeledFaceDescriptors = response.model.map(
+    ({ label, descriptors }) => {
+      return new faceapi.LabeledFaceDescriptors(
+        label,
+        descriptors.map((arr) => {
+          return new Float32Array(arr);
+        })
+      );
+    }
+  );
 
-    console.log("labeledFaceDescriptors: ", labeledFaceDescriptors);
+  console.log('labeledFaceDescriptors: ', labeledFaceDescriptors);
 
-    // localStorage.setItem('labeledFaceDescriptors', JSON.stringify(labeledFaceDescriptors))
+  // localStorage.setItem('labeledFaceDescriptors', JSON.stringify(labeledFaceDescriptors))
 
-    await startVideo();
-    const video = document.getElementById("video");
+  await startVideo();
+  const video = document.getElementById('video');
 
-    // video.addEventListener('playing', () => {
-    const canvas = faceapi.createCanvasFromMedia(video);
-    document.body.append(canvas);
-    const displaySize = { width: video.width, height: video.height };
-    faceapi.matchDimensions(canvas, displaySize);
+  // video.addEventListener('playing', () => {
+  const canvas = faceapi.createCanvasFromMedia(video);
+  document.body.append(canvas);
+  const displaySize = { width: video.width, height: video.height };
+  faceapi.matchDimensions(canvas, displaySize);
 
-    setInterval(async () => {
-        const detections = await faceapi
-            .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
-            .withFaceLandmarks()
-            .withFaceDescriptors()
-            .withFaceExpressions();
-        // console.log(detections)
-        const resizedDetections = faceapi.resizeResults(
-            detections,
-            displaySize
-        );
-        canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
-        // faceapi.draw.drawDetections(canvas, resizedDetections)
-        // faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
-        // faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+  setInterval(async () => {
+    const detections = await faceapi
+      .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+      .withFaceLandmarks()
+      .withFaceDescriptors()
+      .withFaceExpressions();
+    // console.log(detections)
+    const resizedDetections = faceapi.resizeResults(detections, displaySize);
+    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+    // faceapi.draw.drawDetections(canvas, resizedDetections)
+    // faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+    // faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
 
-        const faceMatcher = new faceapi.FaceMatcher(
-            labeledFaceDescriptors,
-            0.6
-        );
-        if (detections) {
-            // detections.forEach(fd => {
-            //     const bestMatch = faceMatcher.findBestMatch(fd.descriptor);
-            //     // const box = resizedDetections.detection.box;
-            //     // const drawBox = new faceapi.draw.DrawBox(box, { label: bestMatch.label });
-            //     // drawBox.draw(canvas);
-            //     console.log(bestMatch.toString())
+    const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6);
+    if (detections) {
+      // detections.forEach(fd => {
+      //     const bestMatch = faceMatcher.findBestMatch(fd.descriptor);
+      //     // const box = resizedDetections.detection.box;
+      //     // const drawBox = new faceapi.draw.DrawBox(box, { label: bestMatch.label });
+      //     // drawBox.draw(canvas);
+      //     console.log(bestMatch.toString())
 
-            // })
-            const results = detections.map((fd) =>
-                faceMatcher.findBestMatch(fd.descriptor)
-            );
+      // })
+      const results = detections.map((fd) =>
+        faceMatcher.findBestMatch(fd.descriptor)
+      );
 
-            results.forEach((bestMatch, i) => {
-                const box = resizedDetections[i].detection.box;
-                const text = bestMatch.toString();
-                const drawBox = new faceapi.draw.DrawBox(box, { label: text });
-                drawBox.draw(canvas);
-            });
-        }
-    }, 100);
-    // });
+      results.forEach((bestMatch, i) => {
+        const box = resizedDetections[i].detection.box;
+        const text = bestMatch.toString();
+        const drawBox = new faceapi.draw.DrawBox(box, { label: text });
+        drawBox.draw(canvas);
+      });
+    }
+  }, 100);
+  // });
 
-    /**
-     * End
-     */
+  /**
+   * End
+   */
 })();
 
 async function startVideo() {
-    //   navigator.getUserMedia(
-    //     { video: {} },
-    //     (stream) => (video.srcObject = stream),
-    //     (err) => console.error(err)
-    //   );
-    if (flvjs.isSupported()) {
-        var videoElement = document.getElementById("video");
-        var flvPlayer = flvjs.createPlayer({
-            type: "flv",
-            isLive: true,
-            url: "http://localhost:8000/live/tinho.flv",
-        });
-        await flvPlayer.attachMediaElement(videoElement);
-        await flvPlayer.load();
-        await flvPlayer.play();
-    }
+  //   navigator.getUserMedia(
+  //     { video: {} },
+  //     (stream) => (video.srcObject = stream),
+  //     (err) => console.error(err)
+  //   );
+  if (flvjs.isSupported()) {
+    var videoElement = document.getElementById('video');
+    var flvPlayer = flvjs.createPlayer({
+      type: 'flv',
+      isLive: true,
+      url: 'http://localhost:8000/live/tinho.flv',
+    });
+    await flvPlayer.attachMediaElement(videoElement);
+    await flvPlayer.load();
+    await flvPlayer.play();
+  }
 }
 
 // async function detectAllLabeledFaces() {
--- routers/mobile.js
+++ routers/mobile.js
@@ -1,33 +1,33 @@
 // require("@tensorflow/tfjs-node");
-const express = require("express");
+const express = require('express');
 const router = new express.Router();
-const sharp = require("sharp");
-const fs = require("fs");
-const path = require("path");
-const formidable = require("formidable");
-const faceapi = require("face-api.js");
-const canvas = require("canvas");
+const sharp = require('sharp');
+const fs = require('fs');
+const path = require('path');
+const formidable = require('formidable');
+const faceapi = require('face-api.js');
+const canvas = require('canvas');
 
 const { Canvas, Image } = canvas;
 faceapi.env.monkeyPatch({ Canvas, Image });
 
-const multer = require("multer");
+const multer = require('multer');
 var storage = multer.memoryStorage(); //store buffer in MemoryStorage
 
 /* Uploaded avatar store in "/avatar" */
 const upload = multer({
-    dest: "temp",
-    limits: {
-        fileSize: 1000000,
-    },
-    fileFilter(req, file, cb) {
-        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
-            return cb(new Error("Please upload: JPEG, PNG, JPG!"));
-        }
+  dest: 'temp',
+  limits: {
+    fileSize: 1000000,
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+      return cb(new Error('Please upload: JPEG, PNG, JPG!'));
+    }
 
-        cb(undefined, true);
-    },
-    storage: storage,
+    cb(undefined, true);
+  },
+  storage: storage,
 });
 
 /* Upload Avatar */
@@ -39,138 +39,155 @@ const upload = multer({
 //     })
 // })
 
-router.post("/mobile/image/:id", (req, res, next) => {
-    const form = new formidable.IncomingForm();
-    form.parse(req, function (err, fields, files) {
-        var oldPath = files.image.path;
-        var folderPath = path.join(__dirname, "../temp") + "/" + req.params.id;
-        var rawData = fs.readFileSync(oldPath);
+router.post('/mobile/image/:id', (req, res, next) => {
+  const form = new formidable.IncomingForm();
+  form.parse(req, function (err, fields, files) {
+    var oldPath = files.image.path;
+    var folderPath =
+      path.join(__dirname, '../public/images') + '/' + req.params.id;
+    var rawData = fs.readFileSync(oldPath);
 
-        if (!fs.existsSync(folderPath)) {
-            fs.mkdirSync(folderPath);
-        }
+    if (!fs.existsSync(folderPath)) {
+      fs.mkdirSync(folderPath);
+    }
 
-        let nextIdx = 0;
+    let nextIdx = fs.readdirSync(folderPath).length + 1;
 
-        fs.readdir(folderPath, (err, files) => {
-            console.log(files.length);
-            nextIdx = files.length + 1;
-        });
+    var newPath = path.join(folderPath, nextIdx + '.jpg');
 
-        var newPath = path.join(folderPath, nextIdx + ".jpg");
-
-        fs.writeFile(newPath, rawData, function (err) {
-            if (err) console.log(err);
-            return res.send("Successfully uploaded");
-        });
+    fs.writeFileSync(newPath, rawData, function (err) {
+      if (err) console.log(err);
     });
-});
 
-router.get("/web/getModel", (req, res, next) => {
-    const model = fs.readFileSync(
-        path.join(__dirname, "../temp") + "/" + "labeledFaceDescriptors.json"
+    /**
+     * add new face
+     */
+    const faceList = JSON.parse(
+      fs.readFileSync(path.join(__dirname, '../temp') + '/' + 'face-list.json')
     );
 
-    res.send({ model: JSON.parse(model) });
+    faceList[req.params.id] = req.params.id;
+
+    fs.writeFileSync(
+      path.join(__dirname, '../temp') + '/' + 'face-list.json',
+      JSON.stringify(faceList)
+    );
+
+    return res.send('Successfully uploaded');
+  });
 });
 
-router.post("/web/trainModel/", async (req, res, next) => {
-    const model = await trainNewModel();
-    res.status(200).send({ message: "successful" });
+router.get('/web/getModel', (req, res, next) => {
+  const model = fs.readFileSync(
+    path.join(__dirname, '../temp') + '/' + 'labeledFaceDescriptors.json'
+  );
+
+  res.send({ model: JSON.parse(model) });
+});
+
+router.post('/web/trainModel/', async (req, res, next) => {
+  const model = await trainNewModel();
+  res.status(200).send({ message: 'successful' });
 });
 
 async function trainNewModel() {
-    // Load model
-    console.log("1");
-    // await faceapi.nets.faceRecognitionNet.loadFromUri("../models");
-    // await faceapi.nets.faceLandmark68Net.loadFromUri("../models");
+  // Load model
+  console.log('1');
+  // await faceapi.nets.faceRecognitionNet.loadFromUri("../models");
+  // await faceapi.nets.faceLandmark68Net.loadFromUri("../models");
 
-    await faceapi.nets.ssdMobilenetv1.loadFromDisk(
-        path.join(__dirname, "../public/models")
-    );
-    await faceapi.nets.tinyFaceDetector.loadFromDisk(
-        path.join(__dirname, "../public/models")
-    );
-    await faceapi.nets.faceLandmark68Net.loadFromDisk(
-        path.join(__dirname, "../public/models")
-    );
-    await faceapi.nets.faceRecognitionNet.loadFromDisk(
-        path.join(__dirname, "../public/models")
-    );
-    await faceapi.nets.faceExpressionNet.loadFromDisk(
-        path.join(__dirname, "../public/models")
-    );
+  await faceapi.nets.ssdMobilenetv1.loadFromDisk(
+    path.join(__dirname, '../public/models')
+  );
+  await faceapi.nets.tinyFaceDetector.loadFromDisk(
+    path.join(__dirname, '../public/models')
+  );
+  await faceapi.nets.faceLandmark68Net.loadFromDisk(
+    path.join(__dirname, '../public/models')
+  );
+  await faceapi.nets.faceRecognitionNet.loadFromDisk(
+    path.join(__dirname, '../public/models')
+  );
+  await faceapi.nets.faceExpressionNet.loadFromDisk(
+    path.join(__dirname, '../public/models')
+  );
 
-    // // Detect Face
-    // const input = document.getElementById("myImg");
+  // // Detect Face
+  // const input = document.getElementById("myImg");
 
-    // const result = await faceapi
-    //   .detectSingleFace(input, new faceapi.SsdMobilenetv1Options())
-    //   .withFaceLandmarks()
-    //   .withFaceDescriptor();
+  // const result = await faceapi
+  //   .detectSingleFace(input, new faceapi.SsdMobilenetv1Options())
+  //   .withFaceLandmarks()
+  //   .withFaceDescriptor();
 
-    //   console.log('3')
-    // const displaySize = { width: input.width, height: input.height };
-    // // resize the overlay canvas to the input dimensions
-    // const canvas = document.getElementById("myCanvas");
-    // faceapi.matchDimensions(canvas, displaySize);
-    // const resizedDetections = faceapi.resizeResults(result, displaySize);
-    // console.log(resizedDetections);
+  //   console.log('3')
+  // const displaySize = { width: input.width, height: input.height };
+  // // resize the overlay canvas to the input dimensions
+  // const canvas = document.getElementById("myCanvas");
+  // faceapi.matchDimensions(canvas, displaySize);
+  // const resizedDetections = faceapi.resizeResults(result, displaySize);
+  // console.log(resizedDetections);
 
-    /**
-     * Real time face detection
-     */
+  /**
+   * Real time face detection
+   */
 
-    // Recognize Face
-    // let labeledFaceDescriptors = JSON.parse(localStorage.getItem('labeledFaceDescriptors'));
-    // console.log('1', labeledFaceDescriptors);
-    // if (!labeledFaceDescriptors){
-    const labeledFaceDescriptors = await detectAllLabeledFaces();
-    console.log("labeledFaceDescriptors: ", labeledFaceDescriptors);
-    // }
-    // localStorage.setItem('labeledFaceDescriptors', JSON.stringify(labeledFaceDescriptors))
+  // Recognize Face
+  // let labeledFaceDescriptors = JSON.parse(localStorage.getItem('labeledFaceDescriptors'));
+  // console.log('1', labeledFaceDescriptors);
+  // if (!labeledFaceDescriptors){
+  const labeledFaceDescriptors = await detectAllLabeledFaces();
+  console.log('labeledFaceDescriptors: ', labeledFaceDescriptors);
+  // }
+  // localStorage.setItem('labeledFaceDescriptors', JSON.stringify(labeledFaceDescriptors))
 
-    fs.writeFileSync(
-        path.join(__dirname, "../temp") + "/" + "labeledFaceDescriptors.json",
-        JSON.stringify(labeledFaceDescriptors)
-    );
+  fs.writeFileSync(
+    path.join(__dirname, '../temp') + '/' + 'labeledFaceDescriptors.json',
+    JSON.stringify(labeledFaceDescriptors)
+  );
 
-    /**
-     * End
-     */
+  /**
+   * End
+   */
 }
 
 async function detectAllLabeledFaces() {
-    const labels = ["Nancy", "Yeonwoo", "Sader", "MaiCuong"];
+  //   const labels = ['Nancy', 'Yeonwoo', 'Sader', 'MaiCuong'];
 
-    // const parentFolder = '../public/images';
+  const labels = Object.keys(
+    JSON.parse(
+      fs.readFileSync(path.join(__dirname, '../temp') + '/' + 'face-list.json')
+    )
+  );
 
-    return Promise.all(
-        labels.map(async (label) => {
-            const descriptions = [];
-            for (let i = 1; i <= 2; i++) {
-                // fs.readdir(parentFolder + '/' + label, (err, files) => {
-                //     files.forEach(async file => {
-                // console.log('file: ', file);
-                // const img = await faceapi.fetchImage(
-                //     `../public/images/${label}/${i}.jpg`
-                // );
-                console.log("2");
-                const img = await canvas.loadImage(
-                    path.join(__dirname, `../public/images/${label}/${i}.jpg`)
-                );
-                console.log("3");
-                const detection = await faceapi
-                    .detectSingleFace(img)
-                    .withFaceLandmarks()
-                    .withFaceDescriptor();
-                descriptions.push(detection.descriptor);
-                // })
-                // })
-            }
-            return new faceapi.LabeledFaceDescriptors(label, descriptions);
-        })
-    );
+  // const parentFolder = '../public/images';
+
+  return Promise.all(
+    labels.map(async (label) => {
+      const descriptions = [];
+      for (let i = 1; i <= 2; i++) {
+        // fs.readdir(parentFolder + '/' + label, (err, files) => {
+        //     files.forEach(async file => {
+        // console.log('file: ', file);
+        // const img = await faceapi.fetchImage(
+        //     `../public/images/${label}/${i}.jpg`
+        // );
+        console.log('2');
+        const img = await canvas.loadImage(
+          path.join(__dirname, `../public/images/${label}/${i}.jpg`)
+        );
+        console.log('3');
+        const detection = await faceapi
+          .detectSingleFace(img)
+          .withFaceLandmarks()
+          .withFaceDescriptor();
+        descriptions.push(detection.descriptor);
+        // })
+        // })
+      }
+      return new faceapi.LabeledFaceDescriptors(label, descriptions);
+    })
+  );
 }
 
 module.exports = router;
