0577be66a6ee40cc2f80f937b750ef88747cf565
Hồ Trọng Tín <medigo.tin.ho@gmail.com>
2021-05-29 21:57:31 +0700

    add pubnub

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,131 +1,148 @@
 (async () => {
-  // Load model
-  console.log('1');
-  // await faceapi.nets.faceRecognitionNet.loadFromUri("../models");
-  // await faceapi.nets.faceLandmark68Net.loadFromUri("../models");
+    // Load model
+    console.log('1');
 
-  await faceapi.nets.ssdMobilenetv1.loadFromUri('../models');
-  await faceapi.nets.tinyFaceDetector.loadFromUri('/models');
-  await faceapi.nets.faceLandmark68Net.loadFromUri('/models');
-  await faceapi.nets.faceRecognitionNet.loadFromUri('/models');
-  await faceapi.nets.faceExpressionNet.loadFromUri('/models');
-  // console.log('2')
-  // // Detect Face
-  // const input = document.getElementById("myImg");
+    /**
+     * Init pubnub instance
+     */
+    const uuid = PubNub.generateUUID();
 
-  // const result = await faceapi
-  //   .detectSingleFace(input, new faceapi.SsdMobilenetv1Options())
-  //   .withFaceLandmarks()
-  //   .withFaceDescriptor();
+    const pubnub = new PubNub({
+        publishKey: "pub-c-a3d37452-ad21-4fa0-93a1-de4900faa04e",
+        subscribeKey: "sub-c-ad92f4ea-c08c-11eb-8415-662615fc053c",
+        uuid
+    });
 
-  //   console.log('3')
-  // const displaySize = { width: input.width, height: input.height };
-  // // resize the overlay canvas to the input dimensions
-  // const canvas = document.getElementById("myCanvas");
-  // faceapi.matchDimensions(canvas, displaySize);
-  // const resizedDetections = faceapi.resizeResults(result, displaySize);
-  // console.log(resizedDetections);
+    await faceapi.nets.ssdMobilenetv1.loadFromUri('../models');
+    await faceapi.nets.tinyFaceDetector.loadFromUri('/models');
+    await faceapi.nets.faceLandmark68Net.loadFromUri('/models');
+    await faceapi.nets.faceRecognitionNet.loadFromUri('/models');
+    await faceapi.nets.faceExpressionNet.loadFromUri('/models');
+    // console.log('2')
+    // // Detect Face
+    // const input = document.getElementById("myImg");
 
-  /**
-   * Real time face detection
-   */
+    // const result = await faceapi
+    //   .detectSingleFace(input, new faceapi.SsdMobilenetv1Options())
+    //   .withFaceLandmarks()
+    //   .withFaceDescriptor();
 
-  // Recognize Face
-  // let labeledFaceDescriptors = JSON.parse(localStorage.getItem('labeledFaceDescriptors'));
-  // console.log('1', labeledFaceDescriptors);
-  // if (!labeledFaceDescriptors){
+    //   console.log('3')
+    // const displaySize = { width: input.width, height: input.height };
+    // // resize the overlay canvas to the input dimensions
+    // const canvas = document.getElementById("myCanvas");
+    // faceapi.matchDimensions(canvas, displaySize);
+    // const resizedDetections = faceapi.resizeResults(result, displaySize);
+    // console.log(resizedDetections);
 
-  let response = await fetch('http://localhost:3002/web/getModel/', {
-    method: 'GET',
-  });
+    /**
+     * Real time face detection
+     */
 
-  response = await response.json();
+    // Recognize Face
+    // let labeledFaceDescriptors = JSON.parse(localStorage.getItem('labeledFaceDescriptors'));
+    // console.log('1', labeledFaceDescriptors);
+    // if (!labeledFaceDescriptors){
 
-  console.log('response: ', response);
-  // }
-  const labeledFaceDescriptors = response.model.map(
-    ({ label, descriptors }) => {
-      return new faceapi.LabeledFaceDescriptors(
-        label,
-        descriptors.map((arr) => {
-          return new Float32Array(arr);
-        })
-      );
-    }
-  );
+    let response = await fetch('http://localhost:3002/web/getModel/', {
+        method: 'GET',
+    });
 
-  console.log('labeledFaceDescriptors: ', labeledFaceDescriptors);
+    response = await response.json();
 
-  // localStorage.setItem('labeledFaceDescriptors', JSON.stringify(labeledFaceDescriptors))
+    console.log('response: ', response);
+    // }
+    const labeledFaceDescriptors = response.model.map(
+        ({ label, descriptors }) => {
+            return new faceapi.LabeledFaceDescriptors(
+                label,
+                descriptors.map((arr) => {
+                    return new Float32Array(arr);
+                })
+            );
+        }
+    );
 
-  await startVideo();
-  const video = document.getElementById('video');
+    console.log('labeledFaceDescriptors: ', labeledFaceDescriptors);
 
-  // video.addEventListener('playing', () => {
-  const canvas = faceapi.createCanvasFromMedia(video);
-  document.body.append(canvas);
-  const displaySize = { width: video.width, height: video.height };
-  faceapi.matchDimensions(canvas, displaySize);
+    // localStorage.setItem('labeledFaceDescriptors', JSON.stringify(labeledFaceDescriptors))
 
-  setInterval(async () => {
-    const detections = await faceapi
-      .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
-      .withFaceLandmarks()
-      .withFaceDescriptors()
-      .withFaceExpressions();
-    // console.log(detections)
-    const resizedDetections = faceapi.resizeResults(detections, displaySize);
-    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
-    // faceapi.draw.drawDetections(canvas, resizedDetections)
-    // faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
-    // faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+    await startVideo();
+    const video = document.getElementById('video');
 
-    const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6);
-    if (detections) {
-      // detections.forEach(fd => {
-      //     const bestMatch = faceMatcher.findBestMatch(fd.descriptor);
-      //     // const box = resizedDetections.detection.box;
-      //     // const drawBox = new faceapi.draw.DrawBox(box, { label: bestMatch.label });
-      //     // drawBox.draw(canvas);
-      //     console.log(bestMatch.toString())
+    // video.addEventListener('playing', () => {
+    const canvas = faceapi.createCanvasFromMedia(video);
+    document.body.append(canvas);
+    const displaySize = { width: video.width, height: video.height };
+    faceapi.matchDimensions(canvas, displaySize);
 
-      // })
-      const results = detections.map((fd) =>
-        faceMatcher.findBestMatch(fd.descriptor)
-      );
+    setInterval(async () => {
+        const detections = await faceapi
+            .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+            .withFaceLandmarks()
+            .withFaceDescriptors()
+            .withFaceExpressions();
+        // console.log(detections)
+        const resizedDetections = faceapi.resizeResults(detections, displaySize);
+        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+        // faceapi.draw.drawDetections(canvas, resizedDetections)
+        // faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+        // faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
 
-      results.forEach((bestMatch, i) => {
-        const box = resizedDetections[i].detection.box;
-        const text = bestMatch.toString();
-        const drawBox = new faceapi.draw.DrawBox(box, { label: text });
-        drawBox.draw(canvas);
-      });
-    }
-  }, 100);
-  // });
+        const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6);
+        if (detections) {
+            // detections.forEach(fd => {
+            //     const bestMatch = faceMatcher.findBestMatch(fd.descriptor);
+            //     // const box = resizedDetections.detection.box;
+            //     // const drawBox = new faceapi.draw.DrawBox(box, { label: bestMatch.label });
+            //     // drawBox.draw(canvas);
+            //     console.log(bestMatch.toString())
 
-  /**
-   * End
-   */
+            // })
+            const results = detections.map((fd) =>
+                faceMatcher.findBestMatch(fd.descriptor)
+            );
+
+            results.forEach((bestMatch, i) => {
+                const box = resizedDetections[i].detection.box;
+                const text = bestMatch.toString();
+
+                if (text == 'unknown') {
+                    pubnub.publish({
+                        channel: "ADRUINO",
+                        message: { 'STATUS': 1 }
+                    })
+                }
+
+                const drawBox = new faceapi.draw.DrawBox(box, { label: text });
+                drawBox.draw(canvas);
+            });
+        }
+    }, 100);
+    // });
+
+    /**
+     * End
+     */
 })();
 
 async function startVideo() {
-  //   navigator.getUserMedia(
-  //     { video: {} },
-  //     (stream) => (video.srcObject = stream),
-  //     (err) => console.error(err)
-  //   );
-  if (flvjs.isSupported()) {
-    var videoElement = document.getElementById('video');
-    var flvPlayer = flvjs.createPlayer({
-      type: 'flv',
-      isLive: true,
-      url: 'http://localhost:8000/live/tinho.flv',
-    });
-    await flvPlayer.attachMediaElement(videoElement);
-    await flvPlayer.load();
-    await flvPlayer.play();
-  }
+    //   navigator.getUserMedia(
+    //     { video: {} },
+    //     (stream) => (video.srcObject = stream),
+    //     (err) => console.error(err)
+    //   );
+    if (flvjs.isSupported()) {
+        var videoElement = document.getElementById('video');
+        var flvPlayer = flvjs.createPlayer({
+            type: 'flv',
+            isLive: true,
+            url: 'http://localhost:8000/live/tinho.flv',
+        });
+        await flvPlayer.attachMediaElement(videoElement);
+        await flvPlayer.load();
+        await flvPlayer.play();
+    }
 }
 
 // async function detectAllLabeledFaces() {
